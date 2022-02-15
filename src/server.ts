35c4a61739f7264c7700fd18ee0c11045fbe847c
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-02-15 20:05:43 -0300

    Criando route e model de categories

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-const app = express();
+import { categoriesRoutes } from './routes/categories.routes';
 
-app.get("/", (req, res) => {
-    return res.json({message: 'Hello'})
-})
+const app = express();
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+
 app.listen(3333, () => console.log("Server is running"));
