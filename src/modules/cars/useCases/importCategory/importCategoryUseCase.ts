2aa00cd5d51ba9d3ccc8fe07392881c4909d2142
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-02-22 21:08:48 -0300

    lendo dados do upload com o csv-parse

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,7 +1,39 @@
-
+import fs from 'fs';
+import {parse} from "csv-parse";
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRespository';
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 class ImportCategoryUseCase {
-    execute(file:any){
-        console.log(file)
+    constructor(private categoriesRepository: CategoriesRepository){}
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+        const stream = fs.createReadStream(file.path);
+        const categories: IImportCategory[] = [];
+
+        const parseFile = parse();
+
+        stream.pipe(parseFile);
+        
+        parseFile.on("data", async (line) => {
+        const [name, description] = line;
+        categories.push({name, description
+        });
+    })
+        .on("end", () => {
+            resolve(categories);
+        })
+        .on("error", (err) => {
+            reject(err);
+        })
+    });
+}
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        console.log(categories);
     }
 }
 
