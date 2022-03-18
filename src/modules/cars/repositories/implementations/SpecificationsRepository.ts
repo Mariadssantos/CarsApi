bdf28cffbb98db0d868606aa2681f1011d151e53
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-03-18 17:15:53 -0300

    up repository Specifications

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,25 +1,20 @@
 import { Specification } from "../../entities/Specification";
 import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecifiationsRepository";
+import { getRepository, Repository } from 'typeorm'
 
 class SpecificationsRepository implements ISpecificationsRepository{
-    private specifications: Specification[];
-
+    private repository: Repository<Specification>;
     constructor(){
-        this.specifications = [];
+        this.repository = getRepository(Specification);
     }
-    create({name, description}: ICreateSpecificationDTO): void {
-        const specification = new Specification();
-        
-        Object.assign(specification, {
-            name,
-            description, 
-            created_at: new Date(),
-        });
-        this.specifications.push(specification);
+
+    async create({name, description}: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({name, description});
+        await this.repository.save(specification);
     }
-    findByName(name: string): Specification {
-    const specification = this.specifications.find((specification) => specification.name === name);
-    return specification;
+    async findByName(name: string): Promise<Specification> {
+        const specification = await this.repository.findOne({name});
+        return specification;
     }
 }
 
