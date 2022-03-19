06d9916acc1749ddb09ff08cd0d880279997c22d
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-03-19 11:51:46 -0300

    create contrato IUsersRepository

---
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -0,0 +1,14 @@
+
+interface ICreateUserDTO {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    driver_license: string;
+}
+
+interface IUsersRepository{
+create(data: ICreateUserDTO): Promise<void>;
+}
+
+export { IUsersRepository }
