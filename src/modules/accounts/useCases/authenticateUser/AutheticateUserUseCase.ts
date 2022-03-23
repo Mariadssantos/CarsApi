a15c191d6252d3b4f2fdda255c1f8fb37db31288
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-03-23 18:38:48 -0300

    create authenticateUserUseCase

---
+++ src/modules/accounts/useCases/authenticateUser/AutheticateUserUseCase.ts
@@ -0,0 +1,51 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        email: string;
+        password: string;
+    },
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email or password incorrect!");
+        }
+
+        const token = sign({}, "22027717e054235a874253db64911e42", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return {
+            user,
+            token,
+        }
+    }
+
+}
+
+export { AuthenticateUserUseCase }
