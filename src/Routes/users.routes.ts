1a4e5bf261b04d277b95da44e746a4190dbb9dca
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-03-30 09:43:59 -0300

    create config upload img avatar

--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
-usersRoutes.patch("/avatar", updateAvatarController.handle);
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateAvatarController.handle);
 
 export { usersRoutes }
--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,8 +10,8 @@ class UsersRepository implements IUsersRepository{
         this.repository = getRepository(User);
     }
 
-    async create({name, password, email, driver_license}: ICreateUserDTO): Promise<void> {
-        const user = this.repository.create({name, password, email, driver_license});
+    async create({name, password, email, driver_license, avatar, id}: ICreateUserDTO): Promise<void> {
+        const user = this.repository.create({name, password, email, driver_license, avatar, id});
         await this.repository.save(user);       
     }
 
